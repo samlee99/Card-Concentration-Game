d27bc6388d5a16111832db98bd2b66c96531b93a
samlee99 <sam1994lee@gmail.com>
2017-03-06 08:12:59 -0800

    Changed the splash screen to go to the main menu instead of straight to the game.

--- app/src/main/java/com/example/samlee/cs245application/SplashActivity.java
+++ app/src/main/java/com/example/samlee/cs245application/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 try {
                     sleep(3000);
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
                     startActivity(intent);
                     finish();
                 } catch (InterruptedException e) {
